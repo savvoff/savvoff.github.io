a7a62cd851dccb4f330a145bc153dd7abf878017
savvoff <vigornom@gmail.com>
2023-03-12 00:09:00 +0200

    Add meta canonical link

--- _nuxt/_plugin-vue_export-helper.fb46bd83.js
+++
@@ -1,1 +0,0 @@
-import{f as u}from"./entry.494dc182.js";function n(t,e){return u()._useHead(t,e)}const c=(t,e)=>{const o=t.__vccOpts||t;for(const[r,s]of e)o[r]=s;return o};export{c as _,n as u};
--- _payload.js
+++ _payload.js
@@ -1,1 +1,1 @@
-export default {data:{},prerenderedAt:1678572022374}
+export default {data:{},prerenderedAt:1678572505835}
