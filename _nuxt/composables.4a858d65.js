cdf12524fb1a580b92c35b7bf974abbec8c0fedf
savvoff <vigornom@gmail.com>
2023-03-12 00:44:20 +0200

    Update color theme; Fix kayak proj photo

--- _nuxt/composables.4a858d65.js
+++
@@ -1,1 +0,0 @@
-import{f as r}from"./entry.23af7dce.js";function t(e,u){return r()._useHead(e,u)}export{t as u};
--- _payload.js
+++ _payload.js
@@ -1,1 +1,1 @@
-export default {data:{},prerenderedAt:1678573353408}
+export default {data:{},prerenderedAt:1678574642421}
